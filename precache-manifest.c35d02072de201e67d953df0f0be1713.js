f4e02d1e4babfa805d5c888b62f9e1069d334e9d
Daniel Khoo <danieljkhoo@gmail.com>
2019-01-02 17:38:20 +0800

    Updates

---
+++ precache-manifest.c35d02072de201e67d953df0f0be1713.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ef468e27d4778ac0610",
+    "url": "/ges/static/js/runtime~main.0ef468e2.js"
+  },
+  {
+    "revision": "9e1b96a0fc4eb61839cb",
+    "url": "/ges/static/js/main.9e1b96a0.chunk.js"
+  },
+  {
+    "revision": "4d8a25e6eb6a69b00d7c",
+    "url": "/ges/static/js/1.4d8a25e6.chunk.js"
+  },
+  {
+    "revision": "9e1b96a0fc4eb61839cb",
+    "url": "/ges/static/css/main.353a68ad.chunk.css"
+  },
+  {
+    "revision": "ee4b66f329a85f2dbaf6acf6ec70b327",
+    "url": "/ges/index.html"
+  }
+];
